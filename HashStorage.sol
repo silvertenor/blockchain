3a9826f927a900fe2d53e28a51b71e5fcad8884a
Paul Cunningham <paulcunningham@145.176.21.198.tigernet.wifi.dyn.clemson.edu>
2022-09-29 14:22:40 -0400

    First Draft of Smart Contract

---
+++ HashStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.0;
+
+contract HashList {
+    // pull in hash number;
+    string hashNumber;
+    struct Hash {
+        // assosiate date/time with hash number
+        string time;
+        string hashNumber;
+    }
+    Hash[] public hash; //array for list of hashes
+    mapping(string => string) public dateToHash; //used to map time to hash, so you can get hash using time
+
+    function store(string memory _hashNumber) public {
+        hashNumber = _hashNumber;
+    }
+
+    function retrieve() public view returns (string memory) {
+        return hashNumber;
+    }
+
+    function addHash(string memory _time, string memory _hashNumber) public {
+        hash.push(Hash(_time, _hashNumber)); //append to  Hash[] array
+        dateToHash[_time] = _hashNumber; //use time to get hash
+    }
+}
