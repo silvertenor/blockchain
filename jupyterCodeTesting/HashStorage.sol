38c15a39a88560758a5a35c48d12514720185872
Devin Lane <silvertenor@icloud.com>
2022-10-13 18:42:21 -0400

    updated interactContract

--- jupyterCodeTesting/HashStorage.sol
+++ jupyterCodeTesting/HashStorage.sol
@@ -3,35 +3,40 @@
 pragma solidity 0.6.0;
 
 contract HashStorage {
-    // Creates structure to hold information from configuration (xml) file
     struct Hash {
+        // assosiate date/time with hash number
         string time;
         string hashNumber;
         string userID;
+        string previousTx;
     }
 
-    Hash hash; // Creates a structure Hash called hash
+    Hash hash;
 
-    // Initiate mapping to use computer ID to find date/time and configuration hash
-    mapping(string => string) public idToHash;
+    mapping(string => string) public idToHash; //used to map id to hash, so you can get hash using time
     mapping(string => string) public idToTime;
 
-    // This function adds values to a structure hash,
-    // that includes time of change, hash of configuration file, and computer ID
     function addHash(
         string memory _time,
         string memory _hashNumber,
-        string memory _userID
+        string memory _userID,
+        string memory _previousTx
     ) public {
-        hash = Hash(_time, _hashNumber, _userID);
-        // Will be used in the future to find hash of configuration file
-        // and time of change from a specific user
-        idToHash[_userID] = _hashNumber;
+        hash = Hash(_time, _hashNumber, _userID, _previousTx);
+        idToHash[_userID] = _hashNumber; //use time to get hash
         idToTime[_userID] = _time;
     }
 
-    // Retrieves the hash number and time of change when called within Python script
-    function retrieve() public view returns (string memory, string memory) {
-        return (hash.hashNumber, hash.time);
+    function retrieve()
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            string memory
+        )
+    {
+        return (hash.hashNumber, hash.time, hash.userID, hash.previousTx);
     }
 }
