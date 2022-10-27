fe34b30c32e314497b917921a941ded66d0c60b4
Devin Lane <silvertenor@icloud.com>
2022-10-27 16:21:14 -0400

    BUG FIXES

--- GUI/backend/loadContract.py
+++ GUI/backend/loadContract.py
@@ -12,29 +12,33 @@ load_dotenv(find_dotenv())
 import hashlib
 
 # Constants
-LOG_TXT_PATH = 'logfiles/workstationLog.txt'
-DEVICE_XML_PATH = 'logfiles/Device.xml'
-MAKO_TCW_PATH = 'logfiles/makoTest2.tcw'
+LOG_TXT_PATH = "logfiles/workstationLog.txt"
+DEVICE_XML_PATH = "logfiles/Device.xml"
+MAKO_TCW_PATH = "logfiles/makoTest2.tcw"
 
 
 # Save compiled code to JSON file
 with open("./compiled_code.json", "r") as file:
     contractInfo = json.load(file)
-    
+
 # Deploy file Prereqs
 # Get bytecode
-bytecode = contractInfo["contracts"]["HashStorage.sol"]["HashStorage"]["evm"]["bytecode"]["object"]
+bytecode = contractInfo["contracts"]["HashStorage.sol"]["HashStorage"]["evm"][
+    "bytecode"
+]["object"]
 
 # get abi
 abi = contractInfo["contracts"]["HashStorage.sol"]["HashStorage"]["abi"]
 w3 = Web3(
-Web3.HTTPProvider("HTTP://127.0.0.1:7545")
+    Web3.HTTPProvider("HTTP://127.0.0.1:7545")
 )  # Get this address from RPC provider in ganache GUI
 chain_id = 1337  # From Network ID of ganache GUI
-my_address = os.getenv("PUBLIC_KEY")  # address to deploy from - one of fake accounts in GUI
+my_address = os.getenv(
+    "PUBLIC_KEY"
+)  # address to deploy from - one of fake accounts in GUI
 private_key = os.getenv("PRIVATE_KEY")  # From key symbol next to account
-contract_address = os.getenv("contract_address") # our contract's address
-hash_storage = w3.eth.contract(address=contract_address, abi=abi) # our contract
+contract_address = os.getenv("contract_address")  # our contract's address
+hash_storage = w3.eth.contract(address=contract_address, abi=abi)  # our contract
 
 
 def fileParser(file):
@@ -42,25 +46,26 @@ def fileParser(file):
     computer_name = None
     config_pushed = False
     config_complete = None
-    with open(file, 'r') as file:
+    with open(file, "r") as file:
         for line in file.readlines():
             # print(line)
             # Extract computer name
-            if 'desktop' in line.lower() and '.' not in line:
-                computer_name = line.split(' ')[-1][:-1]
-            if 'starting publish' in line.lower():
-                pass# print(line)
-            if 'download complete' in line.lower():
+            if "desktop" in line.lower() and "." not in line:
+                computer_name = line.split(" ")[-1][:-1]
+            if "starting publish" in line.lower():
+                pass  # print(line)
+            if "download complete" in line.lower():
                 # print(line)
                 config_pushed = True
-                config_complete = parse(line[:line.index(',')])
-    if computer_name and config_pushed:          
+                config_complete = parse(line[: line.index(",")])
+    if computer_name and config_pushed:
         # print('Computer name: ' + computer_name)
         # print('Configuration changed on: ' + str(config_complete))
         return computer_name, str(config_complete)
 
+
 # Read file in chunks (future-proofing) and generate hash:
-def hashGenerator(file, buffer_size = 65536):
+def hashGenerator(file, buffer_size=65536):
     """
     This function reads a given file in chunks and generates a corresponding
     SHA256 hash.
@@ -83,13 +88,14 @@ def hashGenerator(file, buffer_size = 65536):
     """
     file_hash = hashlib.sha256()
     # Read file as binary
-    with open(file, 'rb') as f:
+    with open(file, "rb") as f:
         chunk = f.read(buffer_size)
         # Keep reading and updating hash as long as there is more data:
         while len(chunk) > 0:
             file_hash.update(chunk)
             chunk = f.read(buffer_size)
     return file_hash
+
 
 # Function to update blockchain
 def updateBlockChain(date, new_hash, computer_id, pvsTx):
@@ -120,7 +126,9 @@ def updateBlockChain(date, new_hash, computer_id, pvsTx):
         my_address
     )  # gives our nonce - number of transactions
     # Store new value for hashNumber:
-    store_transaction = hash_storage.functions.addHash(date, new_hash, computer_id, pvsTx).build_transaction(
+    store_transaction = hash_storage.functions.addHash(
+        date, new_hash, computer_id, pvsTx
+    ).build_transaction(
         {
             "gasPrice": w3.eth.gas_price,
             "chainId": chain_id,
@@ -134,26 +142,24 @@ def updateBlockChain(date, new_hash, computer_id, pvsTx):
     send_store_tx = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
     print("Updated!")
-    print(
-        "New value of hash: " + hash_storage.functions.retrieve().call()[0]
-    )
+    print("New value of hash: " + hash_storage.functions.retrieve().call()[0])
 
 
 def encrypt(paramToEncrypt):
     # Read the key
-    with open('mykey.key', 'rb') as mykey:
+    with open("mykey.key", "rb") as mykey:
         key = mykey.read()
     # Encrypt the value
     f = Fernet(key)
-    encryptedParam = f.encrypt(bytes(paramToEncrypt, 'utf-8'))
+    encryptedParam = f.encrypt(bytes(paramToEncrypt, "utf-8"))
     return encryptedParam
+
 
 def decrypt(paramToDecrypt):
     # Read the key
-    with open('mykey.key', 'rb') as mykey:
+    with open("mykey.key", "rb") as mykey:
         key = mykey.read()
-        
+
     f = Fernet(key)
-    decryptedParam = f.decrypt(paramToDecrypt).decode()
+    decryptedParam = f.decrypt(bytes(paramToDecrypt, "utf-8")).decode()
     return decryptedParam
-    
--- GUI/blockchainInput.py
+++ GUI/blockchainInput.py
@@ -32,60 +32,71 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.table = QtWidgets.QTableView()
         # backend code here
-        
+
         pvsTx = hash_storage.functions.retrieve().call()[3]
         history = []
         while pvsTx:
             tx = w3.eth.get_transaction(pvsTx)
             try:
-                obj, params = hash_storage.decode_function_input(tx['input'])
-                params['_time'] = decrypt(params['_time'])
-                params['_userID'] = decrypt(params['_userID'])
-                pvsTx = params['_previousTx']
+                obj, params = hash_storage.decode_function_input(tx["input"])
+                params["_time"] = decrypt(params["_time"])
+                params["_userID"] = decrypt(params["_userID"])
+                pvsTx = params["_previousTx"]
                 history.append(params)
             except:
-                pvsTx = False     
+                pvsTx = False
         import pandas as pd
+
         df = pd.DataFrame(history)
-        df = df[['_time', '_userID', '_hashNumber', '_previousTx']]
-        df.rename(columns={'_time': 'Date', '_userID': 'User ID', '_hashNumber': 'Hash', '_previousTx': 'Previous Transaction'}, inplace=True)
+        df = df[["_time", "_userID", "_hashNumber", "_previousTx"]]
+        df.rename(
+            columns={
+                "_time": "Date",
+                "_userID": "User ID",
+                "_hashNumber": "Hash",
+                "_previousTx": "Previous Transaction",
+            },
+            inplace=True,
+        )
         # df.set_index('_time', inplace=True)
         headers = []
         data = []
         for item in history:
             row = ()
             for key in item:
-                row += item[key],
+                row += (item[key],)
             data.append(row)
         for item in df.columns:
             headers.append(item)
-#         data = [('2022-07-04 17:49:11',
-#   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
-#   'DESKTOP-V8GI6RV',
-#   '0x2d390a10bb93c044a71734c1e87dfe7569f27cb539e42b16bc02e09087671636'),
-#  ('2022-07-04 17:49:11',
-#   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
-#   'DESKTOP-V8GI6RV',
-#   '0xb7795ef98a132a90f90748a47d5c65e52d8d987f52d85edf1a1601f3a4dd354f'),
-#  ('2022-07-04 17:49:11',
-#   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
-#   'DESKTOP-V8GI6RV',
-#   '0xcf8f64f944e563c500048fded6d131edb8aa137d6d9280c8b858615985f1a3d2'),
-#  ('2022-07-04 17:49:11',
-#   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
-#   'DESKTOP-V8GI6RV',
-#   '0x14627426ba68b49c8c0a9bb409c566dda85773e487178d3b45f43609cd969f1a')]
+        #         data = [('2022-07-04 17:49:11',
+        #   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
+        #   'DESKTOP-V8GI6RV',
+        #   '0x2d390a10bb93c044a71734c1e87dfe7569f27cb539e42b16bc02e09087671636'),
+        #  ('2022-07-04 17:49:11',
+        #   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
+        #   'DESKTOP-V8GI6RV',
+        #   '0xb7795ef98a132a90f90748a47d5c65e52d8d987f52d85edf1a1601f3a4dd354f'),
+        #  ('2022-07-04 17:49:11',
+        #   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
+        #   'DESKTOP-V8GI6RV',
+        #   '0xcf8f64f944e563c500048fded6d131edb8aa137d6d9280c8b858615985f1a3d2'),
+        #  ('2022-07-04 17:49:11',
+        #   '0xfcf9b277a29ca61e5661ff86a2ce053748b0ca0abf7c887a98c78ec84094a149',
+        #   'DESKTOP-V8GI6RV',
+        #   '0x14627426ba68b49c8c0a9bb409c566dda85773e487178d3b45f43609cd969f1a')]
 
         self.model = TableModel(data)
         self.table.setModel(self.model)
 
         self.setCentralWidget(self.table)
 
+
 if __name__ == "__main__":
     import sys
-    app=QtWidgets.QApplication(sys.argv)
+
+    app = QtWidgets.QApplication(sys.argv)
     window2 = QtWidgets.QMainWindow
-    ui =MainWindow()
+    ui = MainWindow()
     # ui.__init__(app)
     ui.show()
     sys.exit(app.exec())
