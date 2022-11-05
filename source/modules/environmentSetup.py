b173cccb60b9de1ad6774c7c9ef54ea48678b612
Devin Lane <silvertenor@icloud.com>
2022-11-05 17:44:42 -0400

    updated small bug

--- source/modules/environmentSetup.py
+++ source/modules/environmentSetup.py
@@ -7,22 +7,26 @@ import logging
 logging.info("Looking for .env file to load in variables...")
 try:
     load_dotenv(find_dotenv())
-
+    project_root = os.getenv("PROJECT_ROOT")  # To change in next step
+    os.chdir(project_root)
     # load secrets
     os.environ["my_address"] = os.getenv(
         "PUBLIC_KEY"
     )  # address to deploy from - one of fake accounts in GUI
     private_key = os.getenv("PRIVATE_KEY")  # From key symbol next to account
-    project_root = os.getenv("PROJECT_ROOT")  # To change in next step
     w3 = Web3(
         Web3.HTTPProvider(os.getenv("WEB3_PROVIDER"))
     )  # Get this address from RPC provider in ganache GUI
     chain_id = int(os.getenv("CHAIN_ID"))  # From Network ID of ganache GUI
-    os.environ["contract_address"] = os.getenv(
-        "CONTRACT_ADDRESS"
-    )  # our contract's address
+    try:
+        os.environ["contract_address"] = os.getenv(
+            "CONTRACT_ADDRESS"
+        )  # our contract's address
+        buttonsAllowed = True
     # Change to proper working directory
-    os.chdir(project_root)
+    except:
+        buttonsAllowed = False
+
 except Exception as e:
     logging.error(
         "Could not find certain environment variables. Please make sure .env is up to date."
